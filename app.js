37175fe92b05b6d0797662c7a57449a8cdd7feb1
israel0 <seyeakinsola@gmail.com>
2023-09-11 15:44:49 +0100

    Root Display Text

--- app.js
+++ app.js
@@ -36,6 +36,10 @@ const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
 
+app.use("/" , (req, res) => {
+     res.send("Basic Test Application!");
+});
+
 //  Graphql
 app.use(
   "/graphql",
